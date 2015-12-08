f8303a869b14a404c152080c4c41ee747b957e85
Douglas C. Santos <douglas@douglascaetano.com>
2015-12-08 12:25:36 -0200

    Made prterr() depend on DEBUG define. Created prtinfo().

--- util.h
+++ util.h
@@ -7,7 +7,19 @@
 
 extern char *__progname;
 
-#define prterr(fmt, args...) fprintf(stderr, "%s: " fmt "\n", __progname, ##args)
+#define DEBUG_INFO 1
+#define DEBUG_ERROR 2
+
+#if DEBUG >= DEBUG_INFO
+# define prtinfo(fmt, args...) fprintf(stderr, fmt "\n", ##args)
+#else
+# define prtinfo(fmt, args...)
+#endif
+#if DEBUG >= DEBUG_ERROR
+# define prterr(fmt, args...) fprintf(stderr, "%s:%d:" fmt "\n", __FILE__, __LINE__, ##args)
+#else
+# define prterr(fmt, args...)
+#endif
 
 #endif /* !__UTIL_H__ */
 
