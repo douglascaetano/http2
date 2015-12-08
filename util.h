cdfe4bfdc73b337d3051650baf66031cf37c15b5
Douglas C. Santos <douglas@douglascaetano.com>
2015-12-08 16:17:00 -0200

    Added new debug function, corrected debug level numbers on defines.

--- util.h
+++ util.h
@@ -7,18 +7,21 @@
 
 extern char *__progname;
 
-#define DEBUG_INFO 1
-#define DEBUG_ERROR 2
+#define DEBUG_INFO 2
+#define DEBUG_ERROR 1
 
 #if DEBUG >= DEBUG_INFO
 # define prtinfo(fmt, args...) fprintf(stderr, fmt "\n", ##args)
 #else
 # define prtinfo(fmt, args...)
 #endif
+
 #if DEBUG >= DEBUG_ERROR
 # define prterr(fmt, args...) fprintf(stderr, "%s:%d:" fmt "\n", __FILE__, __LINE__, ##args)
+# define prterrno(func) fprintf(stderr, func ": %s", strerror(errno))
 #else
 # define prterr(fmt, args...)
+# define prterrno(func)
 #endif
 
 #endif /* !__UTIL_H__ */
